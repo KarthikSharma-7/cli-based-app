aca9ba78a5ddd62311669b229d616fd7d9038213
KarthikSharma-7 <karthiksharma72003@gmail.com>
2023-01-23 09:33:32 +0530

    feature: add update-user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const { registerUser, loginUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  loginUser,
+  updateUser,
+} = require("../controllers/userControllers");
+const validateToken = require("../middleware/validateToken");
 
 router.post("/signup", registerUser);
 router.post("/login", loginUser);
+router.put("/update", validateToken, updateUser);
 
 module.exports = router;
