d68a8c1444c205ceaca588db1796909ff5527291
Ch Sahithi <chsahithi.3812@gmail.com>
2023-01-27 22:09:12 +0530

    chore:fix error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const {
   registerUser,
   loginUser,
   updateUser,
-  getAllUsers,
   resetPassword,
 } = require("../controllers/userControllers");
 const validateToken = require("../middleware/validateToken");
